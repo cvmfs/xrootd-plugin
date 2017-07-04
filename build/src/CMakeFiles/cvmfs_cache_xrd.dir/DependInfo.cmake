b6da6272fb27d29db8722729b23ea8aa30a451e9
Matteo Domenighini <mdomenighini@pcphsft110.cern.ch>
2017-07-04 15:55:48 +0200

    fixed bugs in cache_xrd.cc

--- build/src/CMakeFiles/cvmfs_cache_xrd.dir/DependInfo.cmake
+++ build/src/CMakeFiles/cvmfs_cache_xrd.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/mdomenighini/fresh/src/cvmfs_cache_xrd.cc" "/home/mdomenighini/fresh/build/src/CMakeFiles/cvmfs_cache_xrd.dir/cvmfs_cache_xrd.cc.o"
+  "/home/mdomenighini/plugin_module/src/cvmfs_cache_xrd.cc" "/home/mdomenighini/plugin_module/build/src/CMakeFiles/cvmfs_cache_xrd.dir/cvmfs_cache_xrd.cc.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mdomenighini/fresh/src
+# Install script for directory: /home/mdomenighini/plugin_module/src
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ IF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspeci
          FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/libexec/cvmfs/cache/cvmfs_cache_xrd"
          RPATH "")
   ENDIF()
-  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/libexec/cvmfs/cache" TYPE EXECUTABLE FILES "/home/mdomenighini/fresh/build/src/cvmfs_cache_xrd")
+  FILE(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/libexec/cvmfs/cache" TYPE EXECUTABLE FILES "/home/mdomenighini/plugin_module/build/src/cvmfs_cache_xrd")
   IF(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/libexec/cvmfs/cache/cvmfs_cache_xrd" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/libexec/cvmfs/cache/cvmfs_cache_xrd")
     IF(CMAKE_INSTALL_DO_STRIP)
